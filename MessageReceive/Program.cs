398fbf04d4284080ce5cedff17e966c31c818537
Emre Huylu <emrehuylu10@gmail.com>
2024-12-21 15:45:08 +0300

    Add project files.

---
+++ MessageReceive/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace MessageReceive
+{
+    internal class Program
+    {
+        /// <summary>
+        /// This is where the message capture process from RabbitMQ is performed.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            MessageReceive(); // ReceiveMessage
+        }
+
+        private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+        {
+            // To be able to read as a string.
+            string body = Encoding.UTF8.GetString(e.Body.ToArray());
+            Console.WriteLine(body);
+        }
+
+        private static void MessageReceive() // ReceiveMessage
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://ulelssbx:Xe5xxzNaKMqlJv1pM4-rWhzQbrOAyJZP@rat.rmq2.cloudamqp.com/ulelssbx");
+
+            using var connection = factory.CreateConnection();
+
+            // We need to create a queue again in the same way.
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("Message", true, false, false);
+            // Up to here, since the connection has been made, the same steps as writing a message to the queue are followed.
+
+            // To capture the message in the queue, a different structure needs to be created from here.
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("Message", true, consumer); // We are telling it to listen to the queue named 'Message'.
+
+            consumer.Received += Consumer_Received;
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MessageSend/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace MessageSend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                MessageSend(); // SendMessage
+            }
+        }
+
+        public static void MessageSend() // SendMessage
+        {
+            // Connection to RabbitMQ
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://ulelssbx:Xe5xxzNaKMqlJv1pM4-rWhzQbrOAyJZP@rat.rmq2.cloudamqp.com/ulelssbx"); // RabbitMQ URL
+
+            // The connection is starting.
+            using var connection = factory.CreateConnection();
+
+            // A channel needs to be created.
+            var channel = connection.CreateModel();
+
+            // Channel settings and queue creation.
+            channel.QueueDeclare("Message", true, false, false);
+            // queue name (routing key) - Do you want it to be stored somewhere other than memory? - Can this connection be accessed from other places? - Should auto/deleted be used?
+
+            // The sent message has been created.
+            var mesaj = "Test message";
+
+            // The message needs to be converted to binary format to send - message converted to bytes.
+            var body = Encoding.UTF8.GetBytes(mesaj);
+
+            // Since there are no exchanges, empty routing key, properties are null by default, message
+            channel.BasicPublish(string.Empty, "Message", null, body);
+
+            Console.WriteLine("Message sent.");
+            Console.ReadLine();
+        }
+    }
+}
